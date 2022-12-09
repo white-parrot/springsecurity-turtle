445667de8102b26eae5ec7ffc2d9383ea3030614
Pandey <shreyansh_pandey@baxter.com>
2022-12-09 21:27:52 +0530

    springsecurity-turtle : This Commit is to Create a Project Skeleton

---
+++ springsecurity-demystification/src/main/java/com/runtimeturtle/springsecurity/SpringsecurityDemystificationApplication.java
@@ -0,0 +1,13 @@
+package com.runtimeturtle.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDemystificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityDemystificationApplication.class, args);
+	}
+
+}
---
+++ springsecurity-demystification/src/test/java/com/runtimeturtle/springsecurity/SpringsecurityDemystificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runtimeturtle.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityDemystificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
